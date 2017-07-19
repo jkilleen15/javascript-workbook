2b22e3e0029762326ac672ca8d5ef566d6d7610f
Mac User <macuser@macs-mbp.gateway.pace.com>
2017-07-18 18:21:04 -0700

    add pigLatin hw

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,13 +7,65 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
-function pigLatin(word) {
-
-  // Your code here
-
+function pigLatin (answer) {
+  var word = answer.toLowerCase().trim();
+  for (var i = 0; i < word.length; i++) {
+    var vowel;
+    var firstPart;
+    var secondPart;
+    if (word.indexOf('a') === i) {
+      vowel = word.indexOf('a');
+      firstPart = word.slice(0, vowel);
+      secondPart = word.slice(vowel);
+      if (vowel === 0) {
+        return secondPart + firstPart + 'yay';
+      } else {
+        return secondPart + firstPart + 'ay';
+      }
+    } else if (word.indexOf('e') === i) {
+      vowel = word.indexOf('e');
+      firstPart = word.slice(0, vowel);
+      secondPart = word.slice(vowel);
+      if (vowel === 0) {
+        return secondPart + firstPart + 'yay';
+      } else {
+        return secondPart + firstPart + 'ay';
+      }
+    } else if (word.indexOf('i') === i) {
+      vowel = word.indexOf('i');
+      firstPart = word.slice(0, vowel);
+      secondPart = word.slice(vowel);
+      if (vowel === 0) {
+        return secondPart + firstPart + 'yay';
+      } else {
+        return secondPart + firstPart + 'ay';
+      }
+    } else if (word.indexOf('o') === i) {
+      vowel = word.indexOf('o');
+      firstPart = word.slice(0, vowel);
+      secondPart = word.slice(vowel);
+      if (vowel === 0) {
+        return secondPart + firstPart + 'yay';
+      } else {
+        return secondPart + firstPart + 'ay';
+      }
+    } else if (word.indexOf('u') === i) {
+      vowel = word.indexOf('u');
+      firstPart = word.slice(0, vowel);
+      secondPart = word.slice(vowel);
+      if (vowel === 0) {
+        return secondPart + firstPart + 'yay';
+      } else {
+        return secondPart + firstPart + 'ay';
+      }
+    }
+// var firstPart = word.slice(0, vowel);
+// var secondPart = word.slice(vowel);
+// return secondPart + firstPart + "ay";
+  }
 }
 
+// pigLatin("array");
 
 function getPrompt() {
   rl.question('word ', (answer) => {
@@ -22,7 +74,7 @@ function getPrompt() {
   });
 }
 
-// Tests
+ //Tests
 
 if (typeof describe === 'function') {
 
