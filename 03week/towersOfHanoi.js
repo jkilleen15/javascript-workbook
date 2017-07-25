29eb006313145932cca72889b9e96f2bd20effab
Mac User <macuser@macs-mbp.gateway.pace.com>
2017-07-25 16:17:42 -0700

    add towers of hanoi hw

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -13,33 +13,71 @@ let stacks = {
   c: []
 };
 
-function printStacks() {
-  console.log("a: " + stacks.a);
-  console.log("b: " + stacks.b);
-  console.log("c: " + stacks.c);
+let start = '';
+let end = '';
+
+function printStacks () {
+  console.log('a: ' + stacks.a);
+  console.log('b: ' + stacks.b);
+  console.log('c: ' + stacks.c);
 }
 
-function movePiece() {
+function movePiece (end, start) {
   // Your code here
-
+  // console.log('we made it to move piece!');
+  stacks[end].push(stacks[start].pop());
+  checkForWin();
+// }
 }
 
-function isLegal() {
+function isLegal (start, end) {
   // Your code here
-
+  if (start === '' || end === '') {
+    console.log('Please enter start and end stacks - a, b or c');
+    return false;
+    // if (end.length > 0) {
+      // compare disc values
+  } else if (stacks[end][(stacks[end].length - 1)] < stacks[start][(stacks[start].length - 1)]) {
+      // console.log(end[end.length-1]);
+      // console.log(start[start.length-1]);
+    console.log('Sorry, this move is illegal. Please try again!');
+    return false;
+  } else return true;
 }
 
-function checkForWin() {
+function checkForWin () {
   // Your code here
-
+  //console.log('we made it to checking win');
+  if (stacks.b.length === 4 || stacks.c.length === 4) {
+    console.log('You\'ve done it! Congrats on the win!');
+    return true;
+  } else return false;
 }
 
-function towersOfHanoi(startStack, endStack) {
+function towersOfHanoi (startStack, endStack) {
   // Your code here
-
+ // declare start end variables
+ // check legal
+ // var startStack = startStack;
+ // var endStack = endStack;
+ start = startStack;
+ end = endStack;
+ //console.log('start: ' + start);
+ //console.log('end: ' + end);
+ //console.log(startStack);
+ //console.log(endStack);
+ //console.log((end.length)-1);
+ //console.log((start.length)-1);
+ //console.log(stacks[end].length);
+ //console.log(stacks[start].length);
+ //console.log(end.length);
+ //console.log(start.length);
+ if (isLegal(start, end) === true) {
+   movePiece(end, start);
+ } else return false;
 }
 
-function getPrompt() {
+function getPrompt () {
   printStacks();
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
