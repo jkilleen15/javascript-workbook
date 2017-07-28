3a447b1b056648bc8887572db11e27e686e43205
Mac User <macuser@macs-mbp.gateway.pace.com>
2017-07-27 21:10:11 -0700

    add checkpoint 1 mastermind

--- 03week/mastermind.js
+++ 03week/mastermind.js
@@ -10,8 +10,14 @@ const rl = readline.createInterface({
 let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let hint = '';
+let sol = '';
+var guessStr = '';
+var solStr = '';
+var win = false;
 
 function printBoard() {
+  console.log('Your guesses: ');
   for (let i = 0; i < board.length; i++) {
     console.log(board[i]);
   }
@@ -24,25 +30,72 @@ function generateSolution() {
   }
 }
 
-function getRandomInt(min, max) {
+function getRandomInt (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
+function generateHint (guess) {
+  var bothHit = 0;
+  var charHit = 0;
+  // console.log('sol first char ' + sol[0]);
+  // console.log('guess first char ' + guess[0]);
+  for (var i = 0; i < 4; ++i) {
+    if (sol[i] === guess[i]) {
+      ++bothHit;
+    } else if (sol[i] !== guess[i] && guess.includes(sol[i])) {
+      ++charHit;
+    }
+    hint = (bothHit + '-' + charHit);
+    if (bothHit === 4) {
+      win = true;
+    }
+  }
+  return hint;
 }
 
-function mastermind(guess) {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+function mastermind (guess) {
+  // solution = 'abcd'; // Comment this out to generate a random solution
+    // 1) should register a guess and generate hints
+    // 2) should be able to detect a win
+  solStr = solution;
+  console.log('solution: ' + solution);
+  sol = solution.split('');
+  guessStr = guess;
+  guess = guess.split('');
+  // console.log(sol);
+  // console.log(guess);
+  generateHint(guess);
+  board.push(guessStr + ' ' + hint);
+  if (solStr === guessStr) {
+    console.log('\nYou guessed it! The solution is: ' + guessStr + '!\n');
+    console.log('Want to play again?\n');
+    return 'You guessed it!';
+  }
 }
 
+// Notes from Jessica:
+// I updated my getPrompt() to:
+// 1. Print board guesses and continue only if there is no win
+// 2. If win, clear variables and prompt new game
 
-function getPrompt() {
+function getPrompt () {
   rl.question('guess: ', (guess) => {
     mastermind(guess);
-    printBoard();
-    getPrompt();
+    if (!win === true) {
+      printBoard();
+      getPrompt();
+    } else {
+      board = [];
+      solution = '';
+      letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+      hint = '';
+      sol = '';
+      guessStr = '';
+      solStr = '';
+      win = false;
+      generateSolution();
+      getPrompt();
+    }
   });
 }
 
